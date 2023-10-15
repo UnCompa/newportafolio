4835cc7f52ab88299f41e862e08b8e6d5ff2e039
UnCompa <brandonddxd@gmail.com>
2023-10-15 16:45:55 -0500

    Se mejoro el inicio ademas del header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"Poppins": ['Poppins','system-ui'],
+				"Montserrat": ['Montserrat','system-ui'],
+				"Roboto": ['Roboto','system-ui'],
+				"Inter": ['Inter','system-ui'],
+				"Gabarito": ['Gabarito','system-ui'],
+			},
+			colors: {
+				"primario": "#1A1A2E",
+				"secundario": "#E94560",
+				"terceario": "#0F3460",
+			}
+		},
 	},
 	plugins: [],
 }
